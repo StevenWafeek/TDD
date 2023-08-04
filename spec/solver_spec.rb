90f97530b33f1f75db2bc6cfeebc45551bc099d6
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-08-04 12:05:23 -0300

    Changed N to number to fit style requirements

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../solver.rb'
+require_relative '../solver'
 
 RSpec.describe Solver do
   subject(:solver) { described_class.new }
@@ -14,11 +14,11 @@ RSpec.describe Solver do
 
     it 'returns the correct factorial for positive integers' do
       expect(solver.factorial(5)).to eq(120)
-      expect(solver.factorial(10)).to eq(3628800)
+      expect(solver.factorial(10)).to eq(3_628_800)
     end
 
     it 'raises an ArgumentError for negative integers' do
-      expect { solver.factorial(-5) }.to raise_error(ArgumentError, "Factorial not defined for negative numbers")
+      expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial not defined for negative numbers')
     end
   end
 
@@ -30,22 +30,22 @@ RSpec.describe Solver do
   end
 
   describe '#fizzbuzz' do
-    it 'returns "fizz" when N is divisible by 3' do
+    it 'returns "fizz" when number is divisible by 3' do
       expect(solver.fizzbuzz(3)).to eq('fizz')
       expect(solver.fizzbuzz(9)).to eq('fizz')
     end
 
-    it 'returns "buzz" when N is divisible by 5' do
+    it 'returns "buzz" when number is divisible by 5' do
       expect(solver.fizzbuzz(5)).to eq('buzz')
       expect(solver.fizzbuzz(10)).to eq('buzz')
     end
 
-    it 'returns "fizzbuzz" when N is divisible by 3 and 5' do
+    it 'returns "fizzbuzz" when number is divisible by 3 and 5' do
       expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
       expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
     end
 
-    it 'returns N as a string for other cases' do
+    it 'returns number as a string for other cases' do
       expect(solver.fizzbuzz(7)).to eq('7')
       expect(solver.fizzbuzz(17)).to eq('17')
     end
